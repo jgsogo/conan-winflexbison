f8f4b046d99ea99b29bff8ad59f3cb7157e7db95
Javier G. Sogo <jgsogo@gmail.com>
2018-07-11 20:23:42 +0200

    Debug build needs additional data to run that is not copied to bin directory

--- conanfile.py
+++ conanfile.py
@@ -28,6 +28,9 @@ class WinflexbisonConan(ConanFile):
     def package(self):
         output_path = os.path.join(self.build_folder, self.lib_name, "bin", str(self.settings.build_type))
         self.copy("*", dst="bin", src=output_path)
+        if self.settings.build_type == "Debug":
+            release_path = os.path.join(self.build_folder, self.lib_name, "bin", "Release")
+            self.copy("*", excludes="*.exe", dst="bin", src=release_path)
 
     def package_info(self):
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
